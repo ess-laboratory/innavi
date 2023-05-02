2299c4a87c3e62eee875640dbb8120f8d2d2dd6a
Masahiro Terada <masahiro@mterada.com>
2023-05-03 05:57:59 +0900

    first trial
    検討用のテンプレートサイトを用意する

---
+++ precache-manifest.2f8f4d163dade8d9799335c8ae097740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18a36af8d6be68171160",
+    "url": "./static/css/main.fbaddcda.chunk.css"
+  },
+  {
+    "revision": "18a36af8d6be68171160",
+    "url": "./static/js/main.82419f19.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8f020f1ff5fc478776c7",
+    "url": "./static/js/2.9d40e4c8.chunk.js"
+  },
+  {
+    "revision": "9c841f8f68d4021df1b8d61115c2415d",
+    "url": "./index.html"
+  }
+];
